ead7c81c885a95e92ad30ac8d5a494368cd7c181
Oscar Figueroa <Osc.Fig96@gmail.com>
2020-12-01 23:41:24 -0800

    need a few more

--- js/index.js
+++ js/index.js
@@ -11,26 +11,35 @@ logo.addEventListener('mouseover', event => {
 const keyDown = document.querySelector('body');
 keyDown.addEventListener('keydown', event => {
     if (event.key === 'Control'){
-   console.log("keydown works")}
+   alert("keydown works")}
 });
 
 //-wheel-
-
+window.addEventListener('scroll', event => {
+    console.log(`I have scrolled ${event.deltaMode} number of times.`);
+});
 
 //-load-
-
+window.addEventListener('load',(event) => {
+    alert('Page is fully loaded');
+});
 
 //-focus-
 
 
 //-resize-
-
+window.addEventListener('resize', (event) =>{
+    console.log('Change in size');
+});
 
 //-scroll-
 
 
 //-select-
-
+const selectP = document.querySelector('p')
+selectP.addEventListener('select', (event) => {
+    console.log("You selected some text.");
+});
 
 //-dbclick-
 
