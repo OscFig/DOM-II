1f087482001e7044ada799408d9eb06e27e25db9
Oscar Figueroa <Osc.Fig96@gmail.com>
2020-12-01 17:23:10 -0800

    almost there

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+
+//-mouseover-
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = 'red'
+    // console.log("hello");
+});
+
+//-keydown-
+const keyDown = document.querySelector('body');
+keyDown.addEventListener('keydown', event => {
+    if (event.key === 'Control'){
+   console.log("keydown works")}
+});
+
+//-wheel-
+
+
+//-load-
+
+
+//-focus-
+
+
+//-resize-
+
+
+//-scroll-
+
+
+//-select-
+
+
+//-dbclick-
+
+
+//-drag / drop-
